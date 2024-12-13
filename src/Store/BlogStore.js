e18478c1524b88eb892611d26d720a7e6f12dbcc
Legendearn-01 <lgndearn@gmail.com>
2024-12-13 15:41:31 +0530

    Update BlogStore.js

--- src/Store/BlogStore.js
+++ src/Store/BlogStore.js
@@ -9,6 +9,13 @@ export const useBlogStore = create((set) => ({
       image:
         "https://plus.unsplash.com/premium_photo-1730828574113-201e3352bea6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8",
     },
+    {
+      title: "This is title",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum magni labore sint ea ratione non et, facilis iste nihil unde a, similique voluptates accusamus beatae vero repudiandae sed deserunt voluptatum sunt officia itaque maiores qui amet ipsa. Molestiae ipsa magnam, sint eos cum quae iusto, tempora repudiandae minima quaerat porro.",
+      image:
+        "https://plus.unsplash.com/premium_photo-1730828574113-201e3352bea6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8",
+    },
   ],
   setBlog: (blog) => {
     set({ blog });
